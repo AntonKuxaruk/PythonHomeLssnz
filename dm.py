2ed154ab6c930379288fd20757e993342d50191b
Anton Kuxaruk <anton.kuxaruk@mail.ru>
2019-11-30 23:02:43 +0300

    Flask demo lesson

--- dm.py
+++ dm.py
@@ -1,1 +1,7 @@
-print(1)
+import sys
+
+print(sys.executable)
+
+print('Python %s on %s' % (sys.version, sys.platform))
+#print(sys.path.extend([WORKIn
+# NG_DIR_AND_PYTHON_PATHS]))
---
+++ flask_demo/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hw():
+    #return f'<h2>Hola, {request.args["lastname"]} {request.args["name"]}!</h2>'
+    data = ["Az", "Buki", "Vedi"]
+    return render_template("index.html", name=request.args["name"], lastname=request.args["lastname"], data=data, flag=False)
+
+if __name__ == '__main__':
+    app.run()
